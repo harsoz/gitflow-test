022374e2e9f493378bea4efff8482606a56a507f
harsoz <66538679+harsoz@users.noreply.github.com>
2026-09-18 13:19:49 -0600

    update changing wording

--- page.js
+++ page.js
@@ -3,6 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const mensaje = document.getElementById('mensaje');
 
     boton.addEventListener('click', () => {
-        mensaje.textContent = '¡Excelente! El archivo JavaScript se ha ejecutado correctamente.';
+        mensaje.textContent = 'Excelente, now it isexecuted';
     });
 });
