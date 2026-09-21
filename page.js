ad0a819fb0351f8307ad0575590f9bbf8b957a1d
harsoz <66538679+harsoz@users.noreply.github.com>
2026-09-20 19:02:09 -0600

    adding change 2

--- page.js
+++ page.js
@@ -10,5 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('El DOM ha sido completamente cargado y analizado.');
 
     console.log('This is a test for the new branch: 1');
+    console.log('This is a test for the new branch: 2');
 
 });
